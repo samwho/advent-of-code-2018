46ca242f1c9f89477142721de8f2189cc17240c7
Sam Rose <samwho@lbak.co.uk>
2018-12-12 15:19:44 +0000

    Solution to 9.1 and 9.2.

---
+++ 9.2/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"container/ring"
+	"fmt"
+)
+
+func main() {
+	players := 465
+	lastMarble := 71498 * 100
+
+	scores := make([]int, players, players)
+	marbleValue := 0
+
+	game := ring.New(1)
+	game.Value = marbleValue
+
+	for {
+		marbleValue++
+
+		if marbleValue == lastMarble {
+			break
+		}
+
+		if marbleValue%23 == 0 {
+			player := marbleValue % players
+			scores[player] += marbleValue
+			game = game.Move(-7)
+			scores[player] += game.Value.(int)
+
+			game = game.Prev()
+			game.Unlink(1)
+			game = game.Next()
+
+			continue
+		}
+
+		game = game.Next()
+
+		r := ring.New(1)
+		r.Value = marbleValue
+
+		game = game.Link(r)
+		game = game.Prev()
+	}
+
+	max := 0
+	for _, score := range scores {
+		if score > max {
+			max = score
+		}
+	}
+
+	fmt.Printf("%d\n", max)
+}
